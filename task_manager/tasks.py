51ba8d31a10bec97748b17914d87912097b060b6
gevorgyanAni <anya.gevorgyan.2014@mail.ru>
2024-07-06 15:41:44 +0300

    Done

--- task_manager/tasks.py
+++ task_manager/tasks.py
@@ -2,25 +2,37 @@ import json
 from datetime import datetime
 
 
+class Task:
+    def __init__(self, description, due_date):
+        self.description = description
+        self.creation_date = datetime.now().strftime("%Y-%m-%d")
+        self.due_date = due_date
+        self.status = "невыполнено"
+
+    def completed_tasks(self):
+        self.status = "выполнено"
+
+    def edit_task(self, new_description, new_due_date):
+        self.description = new_description
+        self.due_date = new_due_date
+
+    def __repr__(self):
+        return f"Task(description={self.description}, creation_date={self.creation_date}, due_date={self.due_date}, status={self.status})" #строковое представление объекта
+
+
 class TaskManager:
     def __init__(self):
         self.filename = 'tasks.json'
         self.tasks = []
         self.load_tasks()
 
-    def add_task(self, description, due_date):  #добавляю задчу
-        task = {
-            "description": description,
-            "creation_date": datetime.now().strftime("%Y-%m-%d"),
-            "due_date": due_date,
-            "status": "невыполнено"
-        }
+    def add_task(self, task):
         self.tasks.append(task)
         self.save_tasks()
 
     def completed_tasks(self, index):  #отметка выполнено или не выполнено
         if 0 <= index < len(self.tasks):
-            self.tasks[index]["status"] = "выполнено"
+            self.tasks[index].completed_tasks()
             self.save_tasks()
 
     def delete_task(self, index):  #удаляем задачу
@@ -30,21 +42,22 @@ class TaskManager:
 
     def edit_task(self, index, new_description, new_due_date):  #редактируем задачу
         if 0 <= index < len(self.tasks):
-            self.tasks[index]["description"] = new_description
-            self.tasks[index]["due_date"] = new_due_date
+            self.tasks[index].edit_task(new_description, new_due_date)
             self.save_tasks()
 
     def save_tasks(self):  #сохраняет задачу!!использовать каждый раз
         try:
             with open(self.filename, 'w', encoding='utf-8') as file:
-                json.dump(self.tasks, file, ensure_ascii=False, indent=4)  #сохраняет задачи в файл в формате JSON, отступы в 4 пробела
+                json.dump([task.__dict__ for task in self.tasks], file, ensure_ascii=False,
+                          indent=4)  #сохраняет задачи в файл в формате JSON, отступы в 4 пробела
         except Exception as e:
             print(f"Ошибка при сохранении задач: {e}")
 
     def load_tasks(self):  #работа с файлом, загружает задачи из файла
         try:
             with open(self.filename, 'r', encoding='utf-8') as file:
-                self.tasks = json.load(file)
+                tasks_data = json.load(file)
+                self.tasks = [Task(**task_data) for task_data in tasks_data]
         except FileNotFoundError:
             pass
         except Exception as e:
@@ -57,7 +70,7 @@ def print_tasks(tasks):
     else:
         for i, task in enumerate(tasks):
             print(
-                f"{i+1}. {task['description']}, срок выполнения: {task['due_date']}, статус: {task['status']}, дата создания: {task['creation_date']}")
+                f"{i+1}. {task.description}, срок выполнения: {task.due_date}, статус: {task.status}, дата создания: {task.creation_date}")
 
 
 def main():
@@ -77,7 +90,9 @@ def main():
         if choice == '1':
             description = input("Введите описание задачи: ")
             due_date = input("Введите срок выполнения задачи (в формате ГГГГ-ММ-ДД): ")
-            manager.add_task(description, due_date)
+            task = Task(description, due_date) #создает новый эксемпляр\объект класса
+            manager.add_task(task)
+
         elif choice == '2':
             print_tasks(manager.tasks)
             index = int(input("Введите номер задачи, которую хотите отметить как выполненную: ")) - 1
